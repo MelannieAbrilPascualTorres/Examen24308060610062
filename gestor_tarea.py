86f1189e4b9c7694917ee6de56cd42b55ab4fd3a
MelannieAbrilPascualTorres <24308060610062@cetis61.edu.mx>
2026-04-29 11:08:23 -0600

    num control

--- gestor_tarea.py
+++ gestor_tarea.py
@@ -10,7 +10,7 @@ class GestorTareas:
         try:
             self.cliente = MongoClient(uri, serverSelectionTimeoutMS=5000)
             self.cliente.admin.command('ping')
-            self.db = self.cliente['gestor_tareas']
+            self.db = self.cliente['24308060610062']
             self.tareas = self.db['tareas']
             self.usuarios = self.db['usuarios']
             
